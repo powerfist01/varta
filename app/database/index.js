9349c0ef28e9c75392000e38f9eec129953de3e0
powerfist01 <sujeets207@gmail.com>
2019-11-21 03:32:01 +0530

    Updating with new changes

---
+++ app/database/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var config 		= require('../config');
+var Mongoose 	= require('mongoose');
+var logger 		= require('../logger');
+
+// Connect to the database
+// construct the database URI and encode username and password.
+var dbURI = "mongodb://" + 
+			encodeURIComponent(config.db.username) + ":" + 
+			encodeURIComponent(config.db.password) + "@" + 
+			config.db.host + ":" + 
+			config.db.port + "/" + 
+			config.db.name;
+	dbURI = "mongodb+srv://powerfist01:helloworld@cluster0-5rbft.mongodb.net/test?retryWrites=true&w=majority";
+Mongoose.connect(dbURI, { useNewUrlParser: true });
+
+// Throw an error if the connection fails
+Mongoose.connection.on('error', function(err) {
+	if(err) throw err;
+});
+
+// mpromise (mongoose's default promise library) is deprecated, 
+// Plug-in your own promise library instead.
+// Use native promises
+Mongoose.Promise = global.Promise;
+
+module.exports = { Mongoose, 
+	models: {
+		user: require('./schemas/user.js'),
+		room: require('./schemas/room.js')
+	}
+};
